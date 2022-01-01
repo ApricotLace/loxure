639eead990af063cc0f4e1e8e32b0e135c407a38
ApricotLace <profunctorlense@gmail.com>
2022-01-01 21:40:45 +0300

    Scanner naive impl done

--- src/scanner.clj
+++ src/scanner.clj
@@ -1,4 +1,5 @@
-(ns scanner)
+(ns scanner
+  (:require [clojure.string :as str]))
 
 
 (def token-types
@@ -27,7 +28,10 @@
   (>=  current length))
 
 ;;TODO: Shitty imperative style code, reimplement using parser-combinators or fsm ?
-(defn advance [{:as ctx, :keys [current source]}]
+(defn advance
+  "Consume curr char from stream, inc curr pointer by 1
+  => pair of consumed char & updated ctx"
+  [{:as ctx, :keys [current source]}]
   [(.charAt source current) (update ctx :current inc)]) ;;TODO Smells.
 
 
@@ -60,9 +64,15 @@
 
 
 (defn peek [ctx]
-  (cond
-    (is-at-end? ctx) \0
-    :else (curr-char ctx)))
+  (if (is-at-end? ctx)
+    \0
+    (curr-char ctx)))
+
+
+(defn peek-next [{:as ctx, :keys [current source length]}]
+  (if (>= (inc current) length)
+    \0
+    (.charAt source (inc current))))
 
 
 (defn init-scanner-ctx [source]
@@ -100,9 +110,71 @@
             ctx'))]
     (if (is-at-end? partially-slurped-str-literal-ctx)
       "Throw error :(" #_TODO
-      (let [_ (println partially-slurped-str-literal-ctx)
-            [_ ctx'] (advance partially-slurped-str-literal-ctx)]
+      (let [[_ ctx'] (advance partially-slurped-str-literal-ctx)]
         (add-token ctx' :STRING (trim-quotes ctx'))))))
+
+
+(defn is-digit? [c]
+  (Character/isDigit c))
+
+
+(defn slurp-number-literal [ctx]
+  (let [ctx'
+        (loop [ctx' ctx]
+          (if (is-digit? (peek ctx'))
+            (recur (second (advance ctx')))
+            ctx'))
+
+        {:as ctx', :keys [start current source]}
+        (if (and (= (peek ctx') \.) (is-digit? (peek-next ctx')))
+          (loop [ctx' (second (advance ctx'))]
+            (if (is-digit? (peek ctx'))
+              (recur (second (advance ctx')))
+              ctx'))
+          ctx')]
+
+    (->> (subs source start current)
+         (Double/parseDouble)
+         (add-token ctx' :NUMBER ))))
+
+
+(defn is-alpha? [c]
+  (Character/isLetter c))
+
+
+(defn is-alpha-num? [c]
+  (or (Character/isLetterOrDigit c)
+      (= c \_)))
+
+
+(def keywords
+  {"and"   :AND
+   "class" :CLASS
+   "else"  :ELSE
+   "false" :FALSE
+   "for"   :FOR
+   "fun"   :FUN
+   "if"    :IF
+   "nil"   :NIL
+   "or"    :OR
+   "print" :PRINT
+   "return":RETURN
+   "super" :SUPER
+   "this"  :THIS
+   "true"  :TRUE
+   "var"   :VAR
+   "while" :WHILE})
+
+
+(defn slurp-identifier [ctx]
+  (let [{:as ctx', :keys [source start current]}
+        (loop [ctx' ctx]
+          (if (is-alpha-num? (peek ctx'))
+            (recur (second (advance ctx')))
+            ctx'))
+        text (subs source start current)
+        token-type (if-let [tt (get keywords text)] tt :IDENTIFIER)]
+    (add-token ctx' token-type)))
 
 
 (defn scan-tokens [source]
@@ -120,14 +192,27 @@
           \+ (recur (add-token ctx :PLUS))
           \; (recur (add-token ctx :SEMICOLON))
           \* (recur (add-token ctx :STAR))
-          \! (recur (add-token ctx (if (match ctx \=) :BANG_EQUAL :BANG)))
-          \= (recur (add-token ctx (if (match ctx \=) :EQUAL_EQUAL :EQUAL)))
-          \< (recur (add-token ctx (if (match ctx \=) :LESS_EQUAL :LESS)))
-          \> (recur (add-token ctx (if (match ctx \=) :GREATER_EQUAL :GREATER)))
-          \/ (recur (if (match ctx \/) (omit-comment ctx) (add-token ctx :SLASH)))
+          \! (recur (add-token (if-let [cctx (match ctx \=)]
+                                 cctx ctx) (if (match ctx \=) :BANG_EQUAL :BANG)))
+          \= (recur (add-token (if-let [cctx (match ctx \=)]
+                                 cctx ctx) (if (match ctx \=) :EQUAL_EQUAL :EQUAL))) ;;Uh oh, stinky
+          \< (recur (add-token (if-let [cctx (match ctx \=)]
+                                 cctx ctx) (if (match ctx \=) :LESS_EQUAL :LESS)))
+          \> (recur (add-token (if-let [cctx (match ctx \=)]
+                                 cctx ctx) (if (match ctx \=) :GREATER_EQUAL :GREATER)))
+          \/ (recur (if (match ctx \/) (omit-comment (second (advance ctx))) (add-token ctx :SLASH)))
           \space (recur ctx)
           \newline (recur (inc-lines ctx))
-          \" (recur (slurp-string-literal ctx))))
+          \" (recur (slurp-string-literal ctx))
+          (cond
+            (is-digit? c)
+            (recur (slurp-number-literal ctx))
+
+            (is-alpha? c)
+            (recur (slurp-identifier ctx))
+
+            :error
+            '>?)))
       ctx')))
 
 
@@ -135,7 +220,8 @@
 
   (scan-tokens "// this is a comment
 ((\"Lemon\" \"Apple\")){} // grouping stuff
-!*+-/=<> <= == // operators")
+!*+-/=<> <= == 5490 12.43 // operators
+print \"Hello, World!\"")
 
 
 
